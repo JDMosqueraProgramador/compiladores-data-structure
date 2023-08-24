eda374eca9a2b6c78013711a5c0bdf14e1a2f4f9
nmkdlopez <dylan.lopez@triario.com>
2023-08-23 19:05:42 -0500

    feat: created stack class

---
+++ src/stack.ts
@@ -0,0 +1,25 @@
+
+ class StackNode{
+
+}
+
+ class Stack {
+  private topElement:StackNode | null
+  private size:number
+
+  constructor(){
+    this.topElement = null
+    this.size = 0
+  }
+
+  push(){
+
+  }
+  getDataResult(){
+
+  }
+
+  isEmpty(){
+    return this.size===0
+  }
+}
