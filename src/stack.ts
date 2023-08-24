13d299564fb4a193afb33ab56c27d9aceac16496
nmkdlopez <dylan.lopez@triario.com>
2023-08-23 19:59:11 -0500

    feat: finished stack functionality

--- src/stack.ts
+++ src/stack.ts
@@ -1,38 +1,51 @@
 import { Node } from "./node"
 
- 
+//Clase de PILA
+ export default class Stack<T> {
+   private topElement: Node<T> | null;
+   private size: number;
 
- class Stack<T> {
-  private topElement:Node<T>| null
-  private size:number
+   //AL iniciar, obviamente no tendrá nada.
+   constructor() {
+     this.topElement = null;
+     this.size = 0;
+   }
 
-  constructor(){
-    this.topElement = null
-    this.size = 0
-  }
+   //Cuando se le inserta un dato, se coloca como el "primer elemento".
+   push(data: T) {
+     let newNode = new Node<T>(data);
+     if (!this.isEmpty()) {
+       newNode.setNextNode(this.topElement!);
+     }
+     this.topElement = newNode;
+     this.size++;
+   }
+   //Método que nos devuelve un arreglo de los elementos actuales de la pila, si no hay, devuelve un arreglo vacío.
+   getDataResult() {
+     if (!this.isEmpty()) {
+       let aux = this.topElement;
+       let result = [];
+       for (let index = 0; index < this.size; index++) {
+         result.push(aux!.getData());
+         aux = aux!.getNextNode();
+       }
+       return result;
+     }
+     return [];
+   }
 
-  push(data:any){
-      let newNode = new Node<T>(data)
-      if(!this.isEmpty){
-        newNode.setNextNode(this.topElement!)
-      }
-      this.topElement = newNode
-      this.size++
-  }
-  getDataResult(){
-    if(!this.isEmpty){
-      let aux = this.topElement
-      let result = []
-      for (let index = 0; index < this.size; index++) {
-        result.push(aux!.getData())
-        aux = aux!.getNextNode()
-      }
-      return result
-    }
-    return []
-  }
+   isEmpty() {
+     return this.size === 0;
+   }
 
-  isEmpty(){
-    return this.size===0
-  }
-}
+   //Método que permite quitar el "primer elemento", haciendo que el siguiente sea el nuevo "primer elemento". Si no hay nada, devuelve un mensaje alertando que ya estaba vacía
+   pop() {
+     if (!this.isEmpty()) {
+
+       this.topElement = this.topElement!.getNextNode();
+       this.size--;
+       return;
+     }
+     console.log("The stack is empty");
+   }
+ }
